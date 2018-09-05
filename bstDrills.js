73dc5356be624bbbd9da83748ba00fa94634a0b4
Jennifer Colna <jennifer.colna@gmail.com>
2018-09-05 10:01:43 -0500

    initial commit

---
+++ bstDrills.js
@@ -0,0 +1,21 @@
+// Draw a BST
+// Given the following data 3,1,4,6,9,2,5,7. If you were to insert this into an empty Binary Search Tree, how would the tree look like? (Draw the tree, no coding needed here)
+
+// Remove the root
+// Show how the tree would look like if you deleted the root. (Draw the tree, no coding needed here)
+
+// Create a BST class
+// Walk through the Binary Search Tree code in the curriculum and understand it well. Then write a Binary Search Tree class and with its core functions (insert, remove, find) from scratch.
+
+// Create a Binary Search Tree called BST and insert 3,1,4,6,9,2,5,7 to your tree. Compare your result with the result from the first exercise
+// Height of a BST
+// Write an algorithm to find the height of a binary search tree. What is the run time of your algorithm?
+
+// is it BST?
+// Write an algorithm to check whether an arbitrary binary tree is a binary search tree, assuming the tree does not contain duplicates
+
+// Third largest node
+// Write an algorithm to find the third largest node in a binary search tree
+
+// Balanced BST
+// Write an algorithm that checks if a BST is balanced (i.e. a tree where no two leaves differ in distance from the root by more than one).
